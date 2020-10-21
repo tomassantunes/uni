8d35c02944c0d6e517a2678cfddbc3f276a87e0f
Tomás Antunes <tomasbantunes@gmail.com>
2020-10-21 20:40:54 +0100

    exercício 8 concluído

--- 1ºAno/Semestre_Impar/P1/Exercicios/Code_Condicionais/Condicionais_8.c
+++ 1ºAno/Semestre_Impar/P1/Exercicios/Code_Condicionais/Condicionais_8.c
@@ -2,7 +2,20 @@
 
 int main(){
 
+    int num, numTwo;
 
+    printf("números: ");
+    scanf("%d %d", &num, &numTwo);
 
+    if ( num % numTwo == 0 ) {
+        
+        printf("%d é múltiplo de %d \n", num, numTwo);
+
+    } else
+    {
+        printf("%d não é múltiplo de %d \n", num, numTwo);
+    }
+    
+    
     return 0;
 }
