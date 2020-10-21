04785fb41d784de1ef041b8cc43bfc928da7ca65
Tomás Antunes <tomasbantunes@gmail.com>
2020-10-21 20:34:40 +0100

    exercício 7 concluído

--- 1ºAno/Semestre_Impar/P1/Exercicios/Code_Condicionais/Condicionais_7.c
+++ 1ºAno/Semestre_Impar/P1/Exercicios/Code_Condicionais/Condicionais_7.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
+    float altura;
 
+    printf( "altura: " );
+    scanf( "%f", &altura );
+
+    if ( altura < 1.3 ) {
+
+        printf( "baixíssima! \n" );
+
+    } else if ( altura >= 1.3 && altura < 1.6 ) {
+        
+        printf( "baixa! \n" );
+
+    } else if ( altura >= 1.6 && altura < 1.75 ){
+        
+        printf( "mediana! \n" );
+
+    } else if ( altura >= 1.75 && altura < 1.9 )
+    {
+        
+        printf( "alta! \n" );
+
+    } else if ( altura >= 1.9 ) {
+        
+        printf( "altíssima! \n" );
+
+    } else {
+        
+        printf("pessoa inexistente!");
+
+    }
+    
 
     return 0;
 }
