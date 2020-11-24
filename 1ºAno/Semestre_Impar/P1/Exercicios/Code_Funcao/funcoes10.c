e2466e05f0a60fcb2c5efe1d1f61e70145c829fe
w0rmerdev <73246184+w0rmerdev@users.noreply.github.com>
2020-11-24 20:42:31 +0000

    7-10 exs

--- 1ºAno/Semestre_Impar/P1/Exercicios/Code_Funcao/funcoes10.c
+++ 1ºAno/Semestre_Impar/P1/Exercicios/Code_Funcao/funcoes10.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 
+int haTriangulo(float l1, float l2, float l3) {
+
+	if ( l1 == 0 || l2 == 0 || l3 == 0 ) {
+
+		return  -1;
+
+	} else if ( l1 == l2 && l2 == l3 && ( l1 == 0 || l2 == 0 || l3 == 0 ) ) { 
+
+		return 3; 
+
+	} else if (( l1 == l2 && l2 == l3 && l1 != l3 ) || ( l1 == l2 && l2 != l3 && l1 == l3 ) || ( l1 != l2 && l2 == l3 && l1 == l3 ) && ( l1 == 0 || l2 == 0 || l3 == 0 )) {
+
+		return 2;
+
+	} else if ( l1 != l2 && l2 != l3 && l1 != l3 && ( l1 == 0 || l2 == 0 || l3 == 0 ) ) {
+
+		return 1;
+
+	};
+
+}
+
 int main() {
 
+	float l1, l2, l3, n;
 
+	printf("primeiro lado: ");
+	scanf("%f", &l1);
+
+	printf("segundo lado: ");
+	scanf("%f", &l2);
+
+	printf("terceiro lado: ");
+	scanf("%f", &l3);
+
+	n = haTriangulo(l1, l2, l3);
+	printf("%f\n", n);
 
 	return 0;
 }
--- 1ºAno/Semestre_Impar/P1/Exercicios/Code_Funcao/funcoes9.c
+++ 1ºAno/Semestre_Impar/P1/Exercicios/Code_Funcao/funcoes9.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
 
+int eTriangulo(float l1, float l2, float l3) {
+
+	if ( l1 + l2 > l3 ) { 
+
+		printf("é triângulo\n"); 
+
+	} else if ( l2 + l3 > l1 ) { 
+
+		printf("é triângulo\n");
+
+	} else if ( l3 + l2 > l1 ) { 
+
+		printf("é triângulo\n");
+
+	} else if ( l1 + l3 > l2 ) {
+
+		printf("é triângulo\n");
+
+	} else {
+
+		printf("não é triângulo\n");
+
+	}
+
+	return 0;
+
+}
+
 int main() {
 
+	float l1, l2, l3, n;
 
+	printf("primeiro lado: ");
+	scanf("%f", &l1);
+
+	printf("segundo lado: ");
+	scanf("%f", &l2);
+
+	printf("terceiro lado: ");
+	scanf("%f", &l3);
+
+	n = eTriangulo(l1, l2, l3);
+	printf("%f\n", n);
 
 	return 0;
 }
