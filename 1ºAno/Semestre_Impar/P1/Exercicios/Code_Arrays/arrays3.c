d1d0ead9221933cbb7ed55c8119c1b0d1874c1e7
Tomás Antunes <tomasbanntunes@gmail.com>
2020-12-08 17:23:24 +0000

    arrays something-5

--- 1ºAno/Semestre_Impar/P1/Exercicios/Code_Arrays/arrays3.c
+++ 1ºAno/Semestre_Impar/P1/Exercicios/Code_Arrays/arrays3.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int matrizIdentidade(int matriz[][10], int n) {
+
+	int t = 0;
+
+	for (int i = 0; i < n; i++){
+
+		if(matriz[i][i] == 1){
+
+			t++;
+
+		}
+
+	}
+
+	if(t == n) { return 1; } else { return 0; } 
+
+}
 
 int main() {
 
+	int f;
 
+	int k[][10] = { 
+
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+
+	};
+		
+	f = matrizIdentidade(k, 10);
+	if(f == 1) {
+
+		printf("É matriz identidade. \n");
+
+	} else {
+
+		printf("Não é matriz identidade. \n");
+
+	}
 
 	return 0;
 }
--- 1ºAno/Semestre_Impar/P1/Exercicios/Code_Arrays/arrays5.c
+++ 1ºAno/Semestre_Impar/P1/Exercicios/Code_Arrays/arrays5.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
+int acumulado(int vec[], int sz, int index) {
+
+	int soma = 0;
+
+	for(index; index >= 0; index--){
+
+		soma += vec[index];
+
+	}
+
+	return soma;
+
+}
+
 int main() {
 
+	int f;
+	
+	int array[] = {1, 2, 3};
 
+	f = acumulado(array, 3, 1);
+	printf("O acumulado até ao índice %d é: %d \n", 1, f);
 
 	return 0;
 }
