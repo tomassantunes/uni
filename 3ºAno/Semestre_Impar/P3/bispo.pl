e2e886f95978f096869ca0c05f9221c54c447043
tomassantunes <tomasbantunes@gmail.com>
2023-01-03 13:04:30 +0000

    programa do bispo em prolog

---
+++ 3ºAno/Semestre_Impar/P3/bispo.pl
@@ -0,0 +1,20 @@
+diagonal_direita_para_cima((LX, CX), LI, CI):- LI=<8, CI=<8, LX#=LI, CX#=CI,
+                                               LII is LI+1, CII is CI+1, 
+                                               diagonal_direita_para_cima((LX,CX), LII, CII).
+
+diagonal_direita_para_baixo((LX, CX), LI, CI):- LI>=1, CI=<8, LX#=LI, CX#=CI,
+                                                LII is LI-1, CII is CI+1,
+                                                diagonal_direita_para_baixo((LX, CX), LII, CII).
+
+diagonal_esquerda_para_cima((LX, CX), LI, CI):- LI=<8, CI>=1, LX#=LI, CX#=CI,
+                                                LII is LI+1, CII is CI-1,
+                                                diagonal_esquerda_para_cima((LX, CX), LII, CII).
+
+diagonal_esquerda_para_baixo((LX, CX), LI, CI):- LI>=1, CI>=1, LX#=LI, CX#=CI,
+                                                LII is LI-1, CII is CI-1,
+                                                diagonal_esquerda_para_baixo((LX, CX), LII, CII).
+
+na_bispo((LB, CB), (LX, CX)):- LDC is LB+1, CDC is CB+1, diagonal_direita_para_cima((LX, CX), LDC, CDC),
+                                LDB is LB-1, CDB is CB+1, diagonal_direita_para_baixo((LX, CX),LDB, CDB),
+                                LEC is LB+1, CEC is CB-1, diagonal_esquerda_para_cima((LX, CX), LEC, CEC),
+                                LEB is LB-1, CEB is CB-1, diagonal_esquerda_para_baixo((LX, CX), LEB, CEB).
