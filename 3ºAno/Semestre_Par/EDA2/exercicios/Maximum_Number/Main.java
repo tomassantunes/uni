142525b9e64a13f4b03725608ee01d564c893b00
tomassantunes <tomasbantunes@gmail.com>
2023-02-13 11:01:33 +0000

    pratica 1 eda2

--- 3ºAno/Semestre_Par/EDA2/exercicios/Maximum_Number/Main.java
+++ 3ºAno/Semestre_Par/EDA2/exercicios/Maximum_Number/Main.java
@@ -1,18 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-
-    static int getMax(List<Integer> arr) {
-        int max = arr.get(0);
-
-        for(var x: arr) if(x > max)  max = x;
-
-        return max;
-    }
 
     public static void main(String[] args) throws NumberFormatException, IOException {
 
@@ -20,14 +10,19 @@ public class Main {
 
         int children = Integer.parseInt(input.readLine());
 
-        List<Integer> nums = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
 
         for(int i = 0; i < children; i++) {
             String[] values = input.readLine().split(" ");
 
-            for(int j = 1; j < Integer.parseInt(values[0]); j++) nums.add(Integer.parseInt(values[j]));
+            int sticks = Integer.parseInt(values[0]);
+
+            for(int j = 1; j <= sticks; j++) {
+                int n = Integer.parseInt(values[j]);
+                if(n > max) max = n;
+            }
         }
 
-        System.out.println(getMax(nums));
+        System.out.println(max);
     }
 }
