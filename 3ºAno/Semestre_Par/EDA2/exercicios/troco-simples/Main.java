a6e91908a642856a01102301c300186acc6e76ef
tomassantunes <tomasbantunes@gmail.com>
2023-02-16 12:35:38 +0000

    aula prática de SO e exercicio 2 EDA2

--- 3ºAno/Semestre_Par/EDA2/exercicios/troco-simples/Main.java
+++ 3ºAno/Semestre_Par/EDA2/exercicios/troco-simples/Main.java
@@ -1,24 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.SocketTimeoutException;
 
-public class Main 
-{
-    public static void main(String[] args) throws NumberFormatException, IOException 
-    {
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         
         int nTipos = Integer.parseInt(input.readLine());
 
-        int[] tipos = new int[nTipos];
-
-        String[] linhaTipos = input.readLine().split(" ");
-
-        for(int i = 0; i < nTipos; i++) tipos[i] = Integer.parseInt(linhaTipos[i]);
+        Coins coins = new Coins(nTipos);
+        coins.setValues(input.readLine().split(" "));
 
         int p = Integer.parseInt(input.readLine());
         int[] perguntas = new int[p];
 
         for(int i = 0; i < p; i++) perguntas[i] = Integer.parseInt(input.readLine());
+
+        for(var x: perguntas) {
+            System.out.println(x + ": [" + coins.change(x) + "]");
+        }
+    }
+}
+
+class Coins {
+    private int nCoins;
+    private int[] coins;
+    private int[] mem;
+
+      /* Cria um sistema monetário com COINS moedas diferentes. */
+    public Coins(int coins) {
+        this.nCoins = coins;
+    }
+
+      /* Inicializa os valores das moedas existentes. */
+    public void setValues(String[] values) {
+        this.coins = new int[nCoins];
+
+        for(int i = 0; i < values.length; i++) this.coins[i] = Integer.parseInt(values[i]);
+    }
+
+    /*
+        Calcula e devolve o número mínimo de moedas necessário para
+        obter a quantia AMOUNT.
+    */
+    public int change(int amount) {
+        mem = new int[amount + 1];
+        mem[0] = 0;
+
+        for(int i = 1; i <= amount; i++) {
+            mem[i] = Integer.MAX_VALUE;
+
+            for(int j = 0; j < nCoins; j++) {
+                if(i >= this.coins[j]) {
+                    int min = 1 + mem[i - this.coins[j]];
+                    mem[i] = Math.min(mem[i], min);
+                }
+            }
+        }
+
+        return mem[amount];
     }
 }
---
+++ 3ºAno/Semestre_Par/SO/pratica-1/respostas.md
@@ -0,0 +1,13 @@
+## De que forma o sistema operativo teve que evoluir quando surgiram as primeiras máquinas multiprocessador?
+
+Quando surgiram as primeiras máquinas multiprocessador os sistemas operativos tiveram que evoluir de forma a que fosse possível executar mais do que um processo em determinado momento.
+
+## Como a memória cache influenciou o desenvolvimento de SOs?
+
+A memória cache é uma memória que guarda uma cópia dos dados mais frequentemente acedidos, fazendo com que os acessos a esses dados sejam feitos de maneira mais rápida, poupando tempo de processamento, e melhorando o desempenho do sistema.
+Com a implementação do sistema de cache, os SOs precisaram de gerir a memória da cache e gerir a sincronização entre a cache e a memória principal de forma a melhorar e optimizar o uso da cache.
+
+
+## Como os SOs modernos devem considerar o uso de processadores heterogêneos?
+
+Os SOs modernos devem ser desenvolvidos para suportar processadores heterogéneos de maneira eficiente, com o intuito de melhorar o desempenho e a eficiência do sistema.
