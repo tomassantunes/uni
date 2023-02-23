44d2f6a47f8ff4a286537fd943f75094afea37d6
tomassantunes <tomasbantunes@gmail.com>
2023-02-23 22:30:12 +0000

    problema 2X, problema de tempo

--- 3ºAno/Semestre_Par/EDA2/exercicios/troco/Main.java
+++ 3ºAno/Semestre_Par/EDA2/exercicios/troco/Main.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int nTipos = Integer.parseInt(input.readLine());
 
         Coins coins = new Coins(nTipos);
@@ -14,64 +14,73 @@ public class Main {
         int p = Integer.parseInt(input.readLine());
         int[] perguntas = new int[p];
 
-        for(int i = 0; i < p; i++) perguntas[i] = Integer.parseInt(input.readLine());
+        for (int i = 0; i < p; i++)
+            perguntas[i] = Integer.parseInt(input.readLine());
 
-        for(var x: perguntas) {
-            System.out.println(x + ": [" + coins.change(x) + "]");
+        for (var x : perguntas) {
+            Change y = coins.change(x);
+            y.printOut();
         }
     }
 }
 
+// TOFIX -> Time Limit Exceeded
 class Coins {
     private int nCoins;
     private int[] coins;
 
-      /* Cria um sistema monetário com COINS moedas diferentes. */
     public Coins(int coins) {
         this.nCoins = coins;
     }
 
-      /* Inicializa os valores das moedas existentes. */
     public void setValues(String[] values) {
         this.coins = new int[nCoins];
 
-        for(int i = 0; i < values.length; i++) 
+        for (int i = 0; i < values.length; i++)
             this.coins[i] = Integer.parseInt(values[i]);
     }
 
-    /*
-        Calcula e devolve o número mínimo de moedas necessário para
-        obter a quantia AMOUNT.
-    */
-    public int change(int amount) {
+    public Change change(int amount) {
         int[] mem = new int[amount + 1];
         mem[0] = 0;
 
-        for(int i = 1; i <= amount; i++) {
-            mem[i] = Integer.MAX_VALUE;
+        int[] used = new int[amount + 1];
+        used[0] = 0;
 
-            for(int j = 0; j < nCoins; j++) {
-                if(i >= this.coins[j]) {
-                    int min = 1 + mem[i - this.coins[j]];
-                    mem[i] = Math.min(mem[i], min);
+        for (int i = 1; i <= amount; i++) {
+            mem[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < nCoins; j++) {
+                if (i >= coins[j] && 1 + mem[i - coins[j]] < mem[i]) {
+                    mem[i] = 1 + mem[i - coins[j]];
+                    used[i] = coins[j];
                 }
             }
         }
 
-        return mem[amount];
+        return new Change(amount, mem[amount], used);
+    }
+}
+
+class Change {
+    private int amount;
+    private int num;
+    private int[] used;
+
+    public Change(int amount, int num, int[] used) {
+        this.amount = amount;
+        this.num = num;
+        this.used = used;
     }
 
-    // public int changeRec(int amount) {
-    //     if(amount == 0) {
-    //         return 0;
-    //     } else {
-    //         for(int q = 1; q <= amount; q++) {
-    //             int min = Integer.MAX_VALUE;
-    //             for(int i = 0; i < nCoins; i++) {
-    //                 min = Math.min(q, coins[i] + changeRec(amount - i));
-    //             }
-    //         }
-    //     }
-    //     return min;
-    // }
+    public void printOut() {
+        String nums = " " + used[amount];
+        int tmp = amount - used[amount];
+
+        while (tmp > 0) {
+            nums += " " + used[tmp];
+            tmp -= used[tmp];
+        }
+
+        System.out.println(amount + ": [" + num + "]" + nums);
+    }
 }
