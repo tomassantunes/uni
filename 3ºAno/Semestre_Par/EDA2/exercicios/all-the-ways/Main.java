41874185065b9ed4c6bfcabc773fbabbd94c7b3b
tomassantunes <tomasbantunes@gmail.com>
2023-02-28 10:36:35 +0000

    problema 3 resolvido

--- 3ºAno/Semestre_Par/EDA2/exercicios/all-the-ways/Main.java
+++ 3ºAno/Semestre_Par/EDA2/exercicios/all-the-ways/Main.java
@@ -27,55 +27,51 @@ class Main {
 }
 
 class City {
-    private int[][] city;
-    private String[][] blocks;
+    private long[][] city;
+    private int[][] blocks_horizontal;
+    private int[][] blocks_vertical;
 
     public City(int size) {
-        city = new int[size][size];
-        blocks = new String[size][size];
+        city = new long[size][size];
+        blocks_horizontal = new int[size][size];
+        blocks_vertical = new int[size][size];
     }
     
     public void addBlockage(int x, int y, String Direction) {
-        if(Direction.equals("W")) {
-            blocks[x-2][y-1] = "E";
-            return;
-        }
-
         if(Direction.equals("S")) {
-            blocks[x-1][y-2] = "N";
+            blocks_vertical[x-1][y-2] = 1; // N -> 1
             return;
         }
 
-        blocks[x-1][y-1] = Direction;
+        if(Direction.equals("W")) {
+            blocks_horizontal[x-2][y-1] = 1; // E -> 2
+            return;
+        }
+
+        if(Direction.equals("N")) blocks_vertical[x-1][y-1] = 1;
+        if(Direction.equals("E")) blocks_horizontal[x-1][y-1] = 1;
     }
 
-    public void printCity() {
-        for(var x: city) {
-            for(var y: x) {
-                System.out.print(y + " ");
-            }
-            System.out.println("");
-        }
-    }
-    
-    public int numberOfWays(int sx, int sy, int ex, int ey) {
+    public long numberOfWays(int sx, int sy, int ex, int ey) {
         city[ex-1][ey-1] = 1;
 
         for(int i = ex - 2; i >= sx-1; i--)
-            if(blocks[i][ey-1] == null || !blocks[i][ey-1].equals("E"))
+            if(blocks_horizontal[i][ey-1] != 1)
                 city[i][ey-1] = city[i+1][ey-1];
         
         for(int i = ey - 2; i >= sy-1; i--)
-            if(blocks[ex-1][i] == null || !blocks[ex-1][i].equals("N"))
+            if(blocks_vertical[ex-1][i] != 1)
                 city[ex-1][i] = city[ex-1][i+1];
 
         for(int i = ex - 2; i >= sx-1; i--) {
             for(int j = ey - 2; j >= sy-1; j--) {
-                if(blocks[i][j] == null || !blocks[i][j].equals("E"))
+                if(blocks_horizontal[i][j] != 1) {
                     city[i][j] += city[i+1][j];
+                }
 
-                if(blocks[i][j] == null || !blocks[i][j].equals("N"))
+                if(blocks_vertical[i][j] != 1) {
                     city[i][j] += city[i][j+1];
+                }
             }
         }
 
