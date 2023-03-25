96b19c27ff714214a3ae2113e4027025cc52bff1
tomassantunes <tomasbantunes@gmail.com>
2023-03-25 16:35:49 +0000

    exercicio Project Files feito; Mooshak -> Accepted

--- 3ºAno/Semestre_Par/EDA2/exercicios/project-files/Main.java
+++ 3ºAno/Semestre_Par/EDA2/exercicios/project-files/Main.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
 
 class Main {
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -23,41 +26,67 @@ class Main {
             project.addDependencies(rules);
         }
 
-        project.printTasks();
+        int[] order = project.computeOrder();
+        for(int i = 0; i < tasks - 1; i++) {
+            System.out.print(order[i] + " ");
+        }
+        System.out.println(order[tasks - 1]);
     }
 }
 
 class Project {
     int nodes;
-    int[][] tasks;
+    List<Integer> tasks[];
+    int[] indegree;
 
-    private static enum Colour { WHITE, GREY, BLACK };
-
+    @SuppressWarnings("unchecked")
     public Project(int tasks) {
         this.nodes = tasks;
-        this.tasks = new int[nodes][nodes];
+        this.tasks = new List[nodes];
+        indegree = new int[nodes];
+
+        for(int i = 0; i < nodes; i++) {
+            this.tasks[i] = new LinkedList<>();
+        }
     }
 
     public void addDependency(int task, int precedent) {
-        tasks[task-1][precedent-1] = 1;
+        tasks[task-1].add(precedent-1);
+        indegree[precedent-1]++;
     }
 
     public void addDependencies(int[] rules) {
         for(int i = 2; i < 2 + rules[1]; i++) {
-            addDependency(rules[0], rules[i]);
-        }
-    }
-
-    public void printTasks() {
-        for(var x : tasks) {
-            for(var y : x) {
-                System.out.print(y + " ");
-            }
-            System.out.println("");
+            addDependency(rules[i], rules[0]);
         }
     }
 
     public int[] computeOrder() {
+        PriorityQueue<Integer> Q = new PriorityQueue<>();
+
+        for(int i = 0; i < nodes; i++) {
+            if(indegree[i] == 0) Q.add(i);
+        }
+
+        List<Integer> order = new LinkedList<>();
+        while(!Q.isEmpty()) {
+            int u = Q.remove();
+            order.add(u+1);
+            
+            for(int v : tasks[u]) {
+                indegree[v]--;
+
+                if(indegree[v] == 0) {
+                    Q.add(v);
+                }
+            }
+        }
         
+        int[] result = new int[nodes];
+        for(int i = 0; i < nodes; i++) {
+            result[i] = order.get(i);
+        }
+
+        return result;
     }
 }
