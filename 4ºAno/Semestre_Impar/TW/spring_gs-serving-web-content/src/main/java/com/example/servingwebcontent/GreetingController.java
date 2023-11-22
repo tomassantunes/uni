873cb2ddf9a6c11caef4e46b935885cc30c4153c
Tomás Antunes <tomasbantunes@gmail.com>
2023-11-22 16:01:12 +0000

    aula prática tw

--- 4ºAno/Semestre_Impar/TW/spring_gs-serving-web-content/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ 4ºAno/Semestre_Impar/TW/spring_gs-serving-web-content/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -5,13 +5,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import javax.servlet.http.HttpSession;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class GreetingController {
 
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model, HttpSession session) {
+		System.out.println("SESSION TEST Attribute " + session.getAttribute("x"));
+		List<String> allNames = (List<String>) session.getAttribute("allNames");
+
+		if(allNames == null) {
+			allNames = new LinkedList<String>();
+			session.setAttribute("allNames", allNames);
+		}
+
+		allNames.add(name);
+
 		model.addAttribute("name", name);
+		model.addAttribute("allNames", allNames.toString());
 		return "greeting";
 	}
 
